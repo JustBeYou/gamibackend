44249a26f3deae2fa39d836a422bba0386eae3bf
JustBeYou <mihailferaru2000@gmail.com>
2020-01-11 22:51:05 +0200

    Implemented modify

--- functions/collection/modify.func.js
+++ functions/collection/modify.func.js
@@ -1,15 +1,66 @@
 const express = require('express');
 const permissions = require('../permissions.js');
 const {Collection} = require('../models/collection.js');
+const errorHandlers = require('../errorHandlers.js');
 
 const router = express.Router();
 
+async function updateCollection(id, properties, token, isAdmin) {
+    const collectionRef = await Collection.findOne({
+        where: {
+            id: id,
+        }
+    });
+
+    if (collectionRef === null) {
+        throw new Error('collection not found');
+    }
+
+    if (collectionRef.parentToken != token && !isAdmin) {
+        throw new Error('not enough permissions');
+    }
+
+    if (properties.id !== undefined) {
+        throw new Error('id is immutable');
+    }
+
+    properties.updatedByToken = token;
+    return collectionRef.update(properties);
+}
+
 router.post(
     '/',
-    permissions.necessary([]),
-    (req, res) => {
-        res.json({status: 'ok'});
-    }
+    [
+        permissions.necessary(['ITEMS']),
+        errorHandlers.requestEmptyData,
+    ],
+    async (req, res) => {
+        const isAdmin = permissions.check(req, 'ADMIN_ITEMS');
+
+        let result = null;
+        errorHandlers.safeResponse(res, async () => {
+            if (Array.isArray(req.body.data)) {
+                result = [];
+                for (toUpdate of req.body.data) {
+                    result.push(await updateCollection(
+                        toUpdate.id,
+                        toUpdate.update,
+                        permissions.getReqToken(req),
+                        isAdmin,
+                    ));
+                }
+            } else {
+                result = await updateCollection(
+                    req.body.data.id,
+                    req.body.data.update,
+                    permissions.getReqToken(req),
+                    isAdmin,
+                );
+            }
+
+            res.json({status: 'ok', result: result});
+        });
+    },
 );
 
 module.exports = router;
--- functions/identifier/modify.func.js
+++ functions/identifier/modify.func.js
@@ -1,14 +1,66 @@
 const express = require('express');
 const permissions = require('../permissions.js');
+const {Identifier} = require('../models/identifier.js');
+const errorHandlers = require('../errorHandlers.js');
 
 const router = express.Router();
 
+async function updateIdentifier(id, properties, token, isAdmin) {
+    const identifierRef = await Identifier.findOne({
+        where: {
+            id: id,
+        }
+    });
+
+    if (identifierRef === null) {
+        throw new Error('identifier not found');
+    }
+
+    if (identifierRef.parentToken != token && !isAdmin) {
+        throw new Error('not enough permissions');
+    }
+
+    if (properties.id !== undefined || properties.key !== undefined) {
+        throw new Error('id and key are immutable');
+    }
+
+    properties.updatedByToken = token;
+    return identifierRef.update(properties);
+}
+
 router.post(
     '/',
-    permissions.necessary(['IDENTIFIERS']),
-    (req, res) => {
-        res.json({status: 'ok'});
-    }
+    [
+        permissions.necessary(['IDENTIFIERS']),
+        errorHandlers.requestEmptyData,
+    ],
+    async (req, res) => {
+        const isAdmin = permissions.check(req, 'ADMIN_IDENTIFIERS');
+
+        let result = null;
+        errorHandlers.safeResponse(res, async () => {
+            if (Array.isArray(req.body.data)) {
+                result = [];
+                for (toUpdate of req.body.data) {
+                    result.push(await updateIdentifier(
+                        toUpdate.id,
+                        toUpdate.update,
+                        permissions.getReqToken(req),
+                        isAdmin,
+                    ));
+                }
+            } else {
+                result = await updateIdentifier(
+                    req.body.data.id,
+                    req.body.data.update,
+                    permissions.getReqToken(req),
+                    isAdmin,
+                );
+            }
+
+            res.json({status: 'ok', result: result});
+        });
+    },
 );
 
 module.exports = router;
