4e4d3506eba0f39978f1166da560a0a01e93a381
JustBeYou <mihailferaru2000@gmail.com>
2020-02-14 10:59:40 +0200

    Mock cloud storage.

--- functions/tests/setup.test.js
+++ functions/tests/setup.test.js
@@ -4,6 +4,9 @@ const {initializeIdentifierTables} = require('../models/identifier');
 const {initializeCollectionTables} = require('../models/collection');
 const {initializeModuleTables} = require('../models/module');
 const {initializeFileInfoTables} = require('../models/fileInfo');
+const express = require('express');
+const storage = require('../storage');
+const concat = require('concat-stream');
 
 class TestingDatabase {
     constructor() {
@@ -21,6 +24,72 @@ class TestingDatabase {
     }
 }
 
+class TestingStorage {
+    constructor() {
+        this.storage = {};
+        this.app = express();
+        this.port = 1234;
+
+        const router = express.Router();
+        router.post('/resumable', async (req, res) => {
+            if (req.header('Content-Type') === req.query.type) {
+                this.storage[req.query.bucket][req.query.file] = '';
+                const signelURL = await this.getSignedURL(
+                    req.query.bucket,
+                    req.query.file,
+                    req.query.type,
+                    'write',
+                );
+                res.set('Location', signelURL);
+                res.json({status: 'ok'});
+            } else {
+                res.status(400).json({status: 'wrong type'});
+            }
+        });
+        router.put('/write', (req, res) => {
+            if (req.header('Content-Type') === req.query.type) {
+                this.storage[req.query.bucket][req.query.file] += req.body.toString();
+                res.json({status: 'ok'});
+            } else {
+                res.status(400).json({status: 'wrong type'});
+            }
+        });        
+        router.get('/read', (req, res) => {
+            res.send(this.storage[req.query.bucket][req.query.file]);
+        });
+
+        this.app.use(function(req, res, next){
+            req.pipe(concat(function(data){
+                req.body = data;
+                next();
+            }));
+        });
+        this.app.use(router);
+        this.server = this.app.listen(this.port);
+    }
+
+    createBucket(bucketName) {
+        this.storage[bucketName] = {};
+
+        return Promise.resolve();
+    }
+
+    getSignedURL(bucketName, fileName, fileType, action) {
+        const url = `http://localhost:${this.port}/${action}?bucket=${bucketName}&file=${fileName}&type=${fileType}`;
+        return Promise.resolve(url);
+    }
+
+    checkFileExists(bucketName, fileName) {
+        if (fileName in this.storage[bucketName]) return Promise.resolve(true);
+        return Promise.resolve(false);
+    }
+
+    checkBucketExists(bucketName) {
+        if (bucketName in this.storage) return Promise.resolve(true);
+        else Promise.resolve(false);
+    }
+}
+
 before(async () => {
     console.log('Mocking thrid party services (database, storage, ...)');
 
@@ -32,4 +101,14 @@ before(async () => {
     initializeModuleTables(mockDatabase);
     initializeCollectionTables(mockDatabase);
     await mockDatabase.getConnectionObject().sync({alter: true});
+
+    const mockStorage = new TestingStorage();
+    storage.setDefaultStorage(mockStorage);
+});
+
+after(() => {
+    const mockStorage = storage.getDefaultStorage();
+    if (mockStorage instanceof TestingStorage) {
+        mockStorage.server.close();
+    }
 });
