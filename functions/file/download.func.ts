1a0b8da052dcbbc2626ce222a8dfa840bc970369
JustBeYou <mihailferaru2000@gmail.com>
2020-02-14 10:59:56 +0200

    Upload and download for files.

---
+++ functions/file/download.func.ts
@@ -0,0 +1,29 @@
+import * as permissions from '../permissions';
+import {Request, Response, Router} from 'express';
+import * as errorHandlers from '../errorHandlers';
+import { getDefaultStorage } from '../storage';
+
+const router = Router();
+export default router;
+
+router.post(
+    '/',
+    [
+        permissions.necessary(['ITEMS']),
+        errorHandlers.requestEmptyData,        
+    ],
+    async (req: Request, res: Response) => {
+        const storage = getDefaultStorage();
+
+        await errorHandlers.safeResponse(res, async() => {
+            const url = await storage.getSignedURL(
+                req.body.data.bucket as string,
+                req.body.data.file as string,
+                req.body.data.type as string,
+                "read",
+            );
+
+            res.json({status: 'ok', result: url});
+        });
+    },
+);
