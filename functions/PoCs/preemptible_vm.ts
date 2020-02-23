e3d46eef27a74e1697878a8b47d500c7fda030ad
JustBeYou <mihailferaru2000@gmail.com>
2020-02-23 17:53:04 +0200

    VM creation PoC with startup script.

--- functions/PoCs/preemptible_vm.ts
+++ functions/PoCs/preemptible_vm.ts
@@ -3,20 +3,36 @@ const Compute = require('@google-cloud/compute');
 async function main() {
     const compute = new Compute({
         projectId: 'gamibackend',
-        keyFilename: './serviceAccountKey.json',
+        keyFilename: '../serviceAccountKey.json',
     });
     const zone = compute.zone('us-central1-c');
 
-    const [vm, operation] = await zone.createVM('testvm', {
+    const [vm, operation] = await zone.createVM('fileprocessing', {
         os: 'debian',
-        //http: true,
-        //https: true,
+        http: true,
+        https: true,
         scheduling: {
+            onHostMaintenance: 'TERMINATE',
+            automaticRestart: false,
             preemptible: true,
-        }, 
+        },
+
+        metadata: {
+            items: [
+                {
+                    key: 'startup-script',
+                    value: '#!/bin/sh\n\nsudo apt update\nsudo apt install -y python ffmpeg',
+                },
+            ],
+        kind: 'compute#metadata',
+        },
     });
 
     console.log(vm);
     await operation.promise();
     console.log('created');
 }
+
+main()
+    .then()
+    .catch();
