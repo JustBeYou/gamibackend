c7d62308ad85732332ae4d3629ba95d89bf66e40
JustBeYou <mihailferaru2000@gmail.com>
2020-03-18 19:08:35 +0200

    This function should not be async

--- functions/file/errorHandlers.ts
+++ functions/file/errorHandlers.ts
@@ -1,6 +1,6 @@
 import { FileInfo } from "../models/fileInfo";
 
-export async function validateReference(reference: FileInfo | null, token: string, isAdmin: boolean) {
+export function validateReference(reference: FileInfo | null, token: string, isAdmin: boolean) {
     if (reference === null) throw new Error('File not found.');
     if (reference.parentToken !== token && !isAdmin) throw new Error('Not enough permissions.');
 
