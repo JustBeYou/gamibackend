732d679896f6b625ad90dac1054ba0c9247e9dce
JustBeYou <mihailferaru2000@gmail.com>
2020-03-19 11:21:07 +0200

    Fixed Acess Codes testing

--- functions/tests/api/accessCode.test.js
+++ functions/tests/api/accessCode.test.js
@@ -2,6 +2,7 @@ const chai = require('chai');
 const expect = chai.expect;
 const chaiHttp = require('chai-http');
 const {doFunctionRequest, makeId} = require('../utils');
+const {firestore} = require('../../database');
 
 chai.use(chaiHttp);
 
@@ -36,11 +37,14 @@ describe('Access Code CRUD API', () => {
 
     step('Create', async () => {
         const result = await createAccessCode();
+
         expect(result).to.have.status(200);
+
+        const accessCodeName = result.body.result[0];
+        const accessCodeObject = await firestore.collection('accessCodes').doc(accessCodeName).get();
+        expect(accessCodeObject.exists).to.be.true;
     });
-
-    step('Validate (maybe not necessary)');
-
+    
     step('Search with filters', async () => {
         const created = await createAccessCode();
 
@@ -78,10 +82,84 @@ describe('Access Code CRUD API', () => {
                 ],
             });
 
-        // TODO: accessCode tests are not good enough
         expect(result).to.have.status(200);
+        
+        const accessCodeName = created.body.result[0];
+        const accessCodeObject = await firestore.collection('accessCodes').doc(accessCodeName).get();
+        const acessCodeData = accessCodeObject.data();
+        expect(acessCodeData.maxUsers).to.be.equal(1337);
     });
-    step('Delete');
-    step('Delete all');
-    step('Generate');
+
+    step('Delete', async () => {
+        const created = await createAccessCode();
+        const accessCodeName = created.body.result[0];
+
+        const result = await doFunctionRequest('accessCodeDelete')
+            .setUserToken()
+            .send({
+                data: {
+                    code: accessCodeName,
+                }
+            });
+        expect(result).to.have.status(200);
+
+        const accessCodeObject = await firestore.collection('accessCodes').doc(accessCodeName).get();
+        const accessCodeData = accessCodeObject.data();
+        expect(accessCodeData.type).to.be.equal('INACTIVE');
+    });
+
+    step('Delete all', async () => {
+        const result = await doFunctionRequest('accessCodeDeleteAll')
+            .setUserToken()
+            .send({
+                data: {
+                    CollectionId: parentCollectionId,
+                }
+            });
+        expect(result).to.have.status(200);
+
+        const found = await doFunctionRequest('accessCodeList')
+            .setUserToken()
+            .send({
+                data: {
+                    query: {
+                        id: parentCollectionId,
+                    },
+                },
+            });
+        expect(found.body.result).to.have.length(0);
+    });
+
+    step('Generate', async () => {
+        const result = await doFunctionRequest('accessCodeGenerate')
+            .setUserToken()
+            .send({
+                data: [
+                    {
+                        count: 10,
+                        parentCollection: parentCollectionId,
+                        type: 'PERMANENT',
+                    }
+                ]
+            });
+        expect(result).to.have.status(200);
+        expect(result.body.result[0]).to.have.length(10);
+
+        const found = await doFunctionRequest('accessCodeList')
+            .setUserToken()
+            .send({
+                data: {
+                    query: {
+                        id: parentCollectionId,
+                    },
+                },
+            });
+
+        expect(found.body.result).to.have.length(10);
+        expect(found.body.result).to.satisfy((codesList) => {
+            return codesList.some((code) => {
+                return code.type == 'PERMANENT';
+            });
+        });
+    });
 });
